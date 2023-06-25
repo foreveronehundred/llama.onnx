bd34360eac1664ce004e525e82f33e8f53ae88dd
foreveronehundred <foreveronehundred@hotmail.com>
2023-06-25 14:38:38 +0800

    Update export-onnx.py
    To export ONNX
    
    Signed-off-by: foreveronehundred <foreveronehundred@hotmail.com>

--- tools/export-onnx.py
+++ tools/export-onnx.py
@@ -21,7 +21,7 @@ CACHE_DIR = 'alpaca_out'
 
 class Predictor():
     def __init__(self, outdir):
-        self.device = 'cuda'
+        self.device = 'cpu'
         self.model = LlamaForCausalLM.from_pretrained(outdir, cache_dir=CACHE_DIR, local_files_only=True)
         self.model.to(self.device)
         self.tokenizer = LlamaTokenizer.from_pretrained(outdir, cache_dir=CACHE_DIR, local_files_only=True)
@@ -38,6 +38,14 @@ class Predictor():
         format_prompt = PROMPT.format_map({'instruction': prompt})
         _input = self.tokenizer(format_prompt, return_tensors="pt").input_ids.to(self.device)
 
+        print("Start ONNX conversion")
+        onnx_inputs = _input
+        onnx_filepath = 'MY_PATH/llama7b.onnx'
+        onnx_inp_names = ['token']
+        onnx_out_names = ['output']
+        torch.onnx.export(model=self.model, args=onnx_inputs, f=onnx_filepath, verbose=False, input_names=onnx_inp_names, output_names=onnx_out_names, opset_version=16)
+
+        """
         outputs = self.model.generate(
             _input,
             num_return_sequences=n,
@@ -53,6 +61,7 @@ class Predictor():
         out = [val.split('Response:')[1] for val in out]
         print('Q: {} A: {}'.format(prompt, out))
         return out
+        """
 
 x = Predictor(sys.argv[1])
 x.predict()
